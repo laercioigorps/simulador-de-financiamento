1c7ad7f1415ace4a116717a1a0ecff5dca0efe54
Laercio Souza <laercioigorps@gmail.com>
2022-12-31 08:24:13 -0300

    Adicionado SimulacaoFormulario e tests para o formularion

---
+++ imoveis/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from django.core.exceptions import ValidationError
+import datetime
+
+class SimulacaoFormulario(forms.Form):
+    valor_do_imovel = forms.DecimalField(min_value=0)
+    valor_da_entrada = forms.DecimalField(min_value=0)
+    data_nascimento = forms.DateField()
+    prestacoes = forms.ChoiceField(choices=(("120", "120"),("180", "180"),("240", "240"), ("360", "360")))
+    incluir_ITBI = forms.BooleanField(required=False)
+
+    def clean(self):
+        cleaned_data = super().clean()
+        v_imovel = cleaned_data.get("valor_do_imovel")
+        v_entrada = cleaned_data.get("valor_da_entrada")
+
+        if v_entrada and v_imovel and (v_entrada < (v_imovel * 20 /100)):
+            self.add_error('valor_da_entrada', "Entrada não pode ser menor que 20% do valor do imovel")
+
+    def clean_data_nascimento(self):
+        data_nascimento = self.cleaned_data['data_nascimento']
+        hoje = datetime.date.today()
+        mesno_dia_18_anos_atras = datetime.date(day=hoje.day, month=hoje.month, year=hoje.year-18)
+        if(data_nascimento > mesno_dia_18_anos_atras):
+            raise ValidationError("É necessario ser maior de idade para realizar financiamento!")
---
+++ imoveis/tests/test_forms.py
@@ -0,0 +1,139 @@
+from django.test import TestCase
+from ..forms import SimulacaoFormulario
+import datetime
+
+class FormDeSimulacaoTest(TestCase):
+
+    def setUp(self) -> None:
+        self.validSimulationData = {
+            "data_nascimento" : "09/08/1997",
+            "valor_do_imovel" : 150000,
+            "valor_da_entrada" : 30000,
+            "incluir_ITBI" : False,
+            "prestacoes" : 360,
+        }
+        self.form = SimulacaoFormulario()
+
+    def test_empty_form_validation(self):
+        is_valid = self.form.is_valid()
+        self.assertFalse(is_valid)
+
+    def test_valid_simulation(self):
+        form = SimulacaoFormulario(self.validSimulationData)
+        self.assertTrue(form.is_valid())
+
+#testes do campo de valor do imóvel
+
+    def test_not_valid_price(self):
+        data = self.validSimulationData
+        data["valor_do_imovel"] = "150mil"
+        form = SimulacaoFormulario(self.validSimulationData)
+        self.assertFalse(form.is_valid())
+
+    
+    def test_form_price_decimal_field(self):
+        data = self.validSimulationData
+        data["valor_do_imovel"] = 120.12
+        form = SimulacaoFormulario(data)
+        self.assertTrue(form.is_valid())
+        self.assertEqual(str(form.cleaned_data["valor_do_imovel"]), "120.12")
+
+    
+    def test_form_price_decimal_field(self):
+        data = self.validSimulationData
+        data["valor_do_imovel"] = 120.12
+        form = SimulacaoFormulario(data)
+        self.assertTrue(form.is_valid())
+        self.assertEqual(str(form.cleaned_data["valor_do_imovel"]), "120.12")
+
+    def test_valor_do_imovel_negativo_deve_falhar(self):
+        data = self.validSimulationData
+        data["valor_do_imovel"] = -120.12
+        form = SimulacaoFormulario(data)
+        self.assertFalse(form.is_valid())
+
+#testes do campo de valor da entrada
+
+    def test_valor_da_entrada_invalido(self):
+        data = self.validSimulationData
+        data["valor_da_entrada"] = "30mil"
+        form = SimulacaoFormulario(data)
+        self.assertFalse(form.is_valid())
+
+    def test_valor_da_entrada_menor_que_zero(self):
+        data = self.validSimulationData
+        data["valor_da_entrada"] = -35000
+        form = SimulacaoFormulario(data)
+        self.assertFalse(form.is_valid())
+
+    def test_valor_da_entrada_nao_pode_ser_menor_que_20_porcento_do_total(self):
+        data = self.validSimulationData
+        data["valor_do_imovel"] = 100001
+        data["valor_da_entrada"] = 20000
+        form = SimulacaoFormulario(data)
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors["valor_da_entrada"][0], "Entrada não pode ser menor que 20% do valor do imovel")
+
+ #testes do campo de data de nascimento 
+    def test_data_de_nascimento_formato_invalido(self):
+        data = self.validSimulationData
+        data["data_nascimento"] = "120"
+        form = SimulacaoFormulario(data)
+        self.assertFalse(form.is_valid())
+
+    def test_data_de_nascimento_mais_de_18_anos(self):
+        # date de 17 anos atrás
+        date = datetime.date.today() - datetime.timedelta(weeks=4*12*17)
+        data = self.validSimulationData
+        data["data_nascimento"] = date.strftime("%d/%m/%Y")
+        form = SimulacaoFormulario(data)
+        self.assertFalse(form.is_valid())
+        
+        self.assertEqual(form.errors["data_nascimento"][0], "É necessario ser maior de idade para realizar financiamento!")
+
+    def test_form_sem_data_de_nascimento(self):
+        del self.validSimulationData["data_nascimento"]
+        form = SimulacaoFormulario(self.validSimulationData)
+        self.assertFalse(form.is_valid())
+    
+#testes do campo de prazo de pagamento
+
+    def test_campo_de_prazo_de_pagamento_required(self):
+        del self.validSimulationData["prestacoes"]
+        form = SimulacaoFormulario(self.validSimulationData)
+        self.assertFalse(form.is_valid())
+
+    def test_campo_de_prazo_de_pagamento_com_4_opcoes(self):
+        self.validSimulationData["prestacoes"] = 120
+        form = SimulacaoFormulario(self.validSimulationData)
+        self.assertTrue(form.is_valid())
+
+        self.validSimulationData["prestacoes"] = 180
+        form = SimulacaoFormulario(self.validSimulationData)
+        self.assertTrue(form.is_valid())
+
+        self.validSimulationData["prestacoes"] = 240
+        form = SimulacaoFormulario(self.validSimulationData)
+        self.assertTrue(form.is_valid())
+
+        self.validSimulationData["prestacoes"] = 360
+        form = SimulacaoFormulario(self.validSimulationData)
+        self.assertTrue(form.is_valid())
+
+        self.validSimulationData["prestacoes"] = 121
+        form = SimulacaoFormulario(self.validSimulationData)
+        self.assertFalse(form.is_valid())
+
+#teste campo de financiamento ITBI
+
+    def test_campo_de_ITBI_com_2_opcoes(self):
+        self.validSimulationData["incluir_ITBI"] = "true"
+        form = SimulacaoFormulario(self.validSimulationData)
+        self.assertTrue(form.is_valid())
+
+        del self.validSimulationData["incluir_ITBI"]
+        form = SimulacaoFormulario(self.validSimulationData)
+        self.assertTrue(form.is_valid())
+        self.assertFalse(form.cleaned_data["incluir_ITBI"])
+
+    
